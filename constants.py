62f49cf4b6ccc1b46e56e179e17fa1691786346e
Kartik Ayyer <kartik.ayyer@desy.de>
2023-11-21 15:24:12 +0100

    constants: Added NBUNCHES and DCONF information

--- constants.py
+++ constants.py
@@ -4,11 +4,10 @@ PREFIX = '/gpfs/exfel/exp/SQS/202302/p004098/scratch/'
 
 VDS_DATASET = '/entry_1/instrument_1/detector_1/data'
 
-PROPOSAL = 4098
 DET_NAME = 'SQS_DET_DSSC1M-1'
 MODULE_SHAPE = (128, 512)
-NPULSES = 100
-NCELLS = 400
+NPULSES = 200
+NCELLS = 800
 CHUNK_SIZE = 32
 BAD_CELLIDS = list(range(0,17,2)) + list(range(484,499)) + [810]
 ADU_PER_PHOTON = 5.
@@ -17,3 +16,7 @@ MASK_FNAME = PREFIX + 'geom/badpixel_mask_r0010.h5'
 
 XGM_DA_NUM = 1
 XGM_DATASET = "INSTRUMENT/SQS_DIAG1_XGMD/XGM/DOOCS:output/data/intensitySa3TD"
+NBUNCHES_DA_NUM = 1
+NBUNCHES_DATASET = '/CONTROL/SQS_DIAG1_XGMD/XGM/DOOCS/pulseEnergy/numberOfSa3BunchesActual/value'
+DCONF_DA_NUM = 1
+DCONF_DATASET = '/RUN/SQS_NQS_DSSC/FPGA/PPT_Q1/epcRegisterFilePath/value'
