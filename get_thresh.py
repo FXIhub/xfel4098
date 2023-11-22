870540cb7a829811a18253ace1015c4e20f82f8b
Kartik Ayyer <kartik.ayyer@desy.de>
2023-11-22 15:20:40 +0100

    get_thresh: Fixed get_thresh_all. Added custom NCELLS, NPULSES option

--- get_thresh.py
+++ get_thresh.py
@@ -6,8 +6,8 @@ import h5py
 import numpy as np
 from scipy import ndimage, optimize
 
-from constants import NCELLS, PREFIX
-
+from constants import NCELLS, PREFIX, NPULSES
+LOCAL_SUBT = False
 
 def _get_litpix(run, normed=False):
     # Get lit pixels
@@ -19,26 +19,29 @@ def _get_litpix(run, normed=False):
             xgm[np.where(xgm==0)[0]] = xgm[np.where(xgm==0)[0] - NCELLS].copy()
 
             litpix = litpix / xgm
-            litpix -= ndimage.median_filter(litpix.reshape(-1,NCELLS), (50,5)).ravel()
+            litpix[~np.isfinite(litpix)] = 0
+            litpix[xgm < 2] = 0
+            if LOCAL_SUBT:
+                litpix -= ndimage.median_filter(litpix.reshape(-1,NCELLS), (50,1)).ravel()
 
     return litpix.reshape(-1,NCELLS)
 
 def get_thresh_all(run, return_litpix=False, normed=False, verbose=False):
-    sel_litpix = _get_litpix(run, normed=normed)
+    sel_litpix = _get_litpix(run, normed=normed)[:,:NPULSES]
 
     # Get hit indices
     def gaussian(x, a, x0, sigma):
         return a * np.exp(-(x-x0)**2 / 2 / sigma**2)
 
-    binvals = np.arange(-1,1,0.001) if normed else np.arange(100,10000,10)
-    chists = np.array([np.histogram(sel_litpix[:,i], bins=binvals)[0] for i in range(NCELLS)])
+    if LOCAL_SUBT:
+        binvals = np.arange(-1,10,0.01) if normed else np.arange(100,10000,10)
+    else:
+        binvals = np.arange(0.01,10,0.01) if normed else np.arange(100,10000,10)
+    hy = np.histogram(sel_litpix.ravel(), bins=binvals)[0]
     hcen = (binvals[1:] + binvals[:-1]) * 0.5
     thresh = np.ones(NCELLS) * -100.
-    hcen = np.concatenate([hcen] * thresh.shape[0])
-    indices = np.argsort(hcen)
-    hcen = hcen[indices]
-    hy = chists.ravel()[indices]
-    xmax = np.abs(binvals).argmin() if normed else hy.argmax() + 1 # Ignoring first bin
+    #xmax = np.abs(hcen).argmin() if normed else hy.argmax() + 1 # Ignoring first bin
+    xmax = hy.argmax() if normed else hy.argmax() + 1 # Ignoring first bin
     try:
         p0_std = 0.01 if normed else 100
         popt, pcov = optimize.curve_fit(gaussian,
@@ -47,10 +50,8 @@ def get_thresh_all(run, return_litpix=False, normed=False, verbose=False):
         thresh[:] = popt[1] + 4*np.abs(popt[2])
         if verbose:
             print('Fitted background Gaussian: %.3f +- %.3f' % (popt[1], popt[2]))
-    except RuntimeError:
-        print('Fitting failed for cell %d' % i)
-    except ValueError:
-        print('Fitting failed for cell %d' % i)
+    except (RuntimeError, ValueError):
+        print('Fitting failed')
 
     if return_litpix:
         return thresh, sel_litpix
@@ -73,7 +74,6 @@ def get_thresh(run, return_litpix=False, normed=False, verbose=False):
         xmax = np.abs(binvals).argmin() if normed else hy[1:].argmax() + 1 # Ignoring first bin
         try:
             p0_std = 0.01 if normed else 100
-            # print(hcen, hy)
             popt, pcov = optimize.curve_fit(gaussian,
                                             hcen[1:xmax], hy[1:xmax],
                                             p0=(hy.max(), hcen[xmax], p0_std))
@@ -107,19 +107,28 @@ def get_hitinds(run, thresh, litpix=None, normed=False, verbose=False):
     if litpix is None:
         litpix = _get_litpix(run, normed=normed)
     hit_inds = np.where((litpix > thresh).ravel())[0]
+    hit_inds = hit_inds[hit_inds % NCELLS <= NPULSES]
     if verbose:
-        print('%d hits using a threshold range of %.3f - %.3f' % (len(hit_inds), thresh.min(), thresh.max()))
+        print('%d hits using a threshold range of %.3f - %.3f (%.2f %%)' % (len(hit_inds), thresh.min(), thresh.max(), len(hit_inds) / litpix.size * 100))
     return hit_inds
 
 def main():
+    global NCELLS, NPULSES
     parser = argparse.ArgumentParser(description='Save hits to emc file')
     parser.add_argument('run', help='Run number', type=int)
     parser.add_argument('-N', '--norm', help='Normalize by pulse energy', action='store_true')
+    parser.add_argument('--ncells', help='Override constant NCELLS', type=int)
+    parser.add_argument('--npulses', help='Override constant NPULSES', type=int) 
     args = parser.parse_args()
 
-    thresh, litpix = get_thresh_all(args.run, normed=args.norm, verbose=True, return_litpix=True)
+    if args.ncells is not None:
+        NCELLS = args.ncells
+    if args.npulses is not None:
+        NPULSES = args.npulses
+    thresh = get_thresh_all(args.run, normed=args.norm, verbose=True, return_litpix=False)
     linthresh = linearize(thresh, verbose=True)
-    hitinds = get_hitinds(args.run, linthresh, litpix=litpix, normed=args.norm, verbose=True)
+    hitinds = get_hitinds(args.run, linthresh, normed=args.norm, verbose=True)
+    #print(np.unique(hitinds%NCELLS, return_counts=True))
 
 if __name__ == '__main__':
     main()
