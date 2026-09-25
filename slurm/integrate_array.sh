df14b5736cf8777d5dbd7810c36fb0b288ae91e0
Kartik Ayyer <kartik.ayyer@desy.de>
2026-09-25 12:08:05 +0200

    integrate_array: Fixed MPI module stuff

--- slurm/integrate_array.sh
+++ slurm/integrate_array.sh
@@ -3,7 +3,7 @@
 #SBATCH --array=
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=64
-#SBATCH --time=04:00:00
+#SBATCH --time=12:00:00
 #SBATCH --export=ALL
 #SBATCH -J flag_integrate
 #SBATCH -o .int-%4a-%j.out
@@ -14,14 +14,17 @@
 ####SBATCH --reservation=upex_002995
 
 source /etc/profile.d/modules.sh
-module purge
-source ../source_this
-module load exfel openmpi-no-python
+#module purge
+#source ../source_this
+#module load exfel openmpi-no-python
+module load maxwell openmpi/5.0.3
 
 PREFIX=/gpfs/exfel/exp/SQS/202302/p004098/
 
 run=`printf %.4d "${SLURM_ARRAY_TASK_ID}"`
 flag_file=${PREFIX}/scratch/events/r${run}_events.h5
 
-mpirun -mca btl_tcp_if_include ib0 python ../integrate.py $run -f $flag_file --num_cells=600
+
+#mpirun -mca btl_tcp_if_include ib0 python ../integrate.py $run -f $flag_file --num_cells=600
+mpirun -mca btl_tcp_if_include ib0 python ../integrate.py $run -f $flag_file --num_cells=725
 #mpirun -mca btl_tcp_if_include ib0 python ../integrate.py $run -c 1,326,1
